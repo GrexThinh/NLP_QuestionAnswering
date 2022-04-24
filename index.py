c9d4acc615e26c831c7ffb9e3326a5c12e69a98e
unknown <nguyenminhtancute12a1@gmail.com>
2022-04-24 17:22:07 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from test import answerQuestionMachine
 
 app=Flask("app")
 
 @app.route('/', methods=["POST", "GET"])
-def home():
+def main():
     if request.method=="POST":
         question=request.form["question"]
-        print(question)
-        answer=answerQuestionMachine(question)
-        return render_template('index.html', ques=question, ans=answer)
+        ans=answerQuestionMachine(question)
+        return render_template('index1.html', content=ans)
     else:
-        return render_template('index.html')
+        return render_template('index1.html',content = "Hello World", content1 = "Goodbye")
 
 @app.route('/about')
 def about():
     return "THÔNG TIN NHÓM"
+
+@app.route('/home')
+def home():
+    return redirect(url_for('main'))
 
 if __name__ == "__main__":
     app.run(debug=True)
