70df7eccfed91b6c3568e114b0383579715e81a6
unknown <nguyenminhtancute12a1@gmail.com>
2022-05-11 08:20:17 +0700

    latest update

--- QA.py
+++ QA.py
@@ -17,6 +17,120 @@
 # from transformers import pipeline
 
 
+import concurrent.futures
+import itertools
+import operator
+import re
+import os
+import spacy
+
+import requests
+import wikipedia
+from gensim.summarization.bm25 import BM25
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, QuestionAnsweringPipeline
+
+
+class DocumentRetrieval:
+    
+    def __init__(self, url='https://en.wikipedia.org/w/api.php'):
+        self.url = url
+
+    def search_pages(self, query):
+        params = {
+            'action': 'query',
+            'list': 'search',
+            'srsearch': query,
+            'format': 'json'
+        }
+        res = requests.get(self.url, params=params)
+        return res.json()
+
+    def search_page(self, page_id):
+        res = wikipedia.page(pageid=page_id)
+        return res.content
+
+    def search(self, query):
+        pages = self.search_pages(query)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            process_list = [executor.submit(self.search_page, page['pageid']) for page in pages['query']['search']]
+            docs = [self.post_process(p.result()) for p in process_list]
+        return docs
+
+    def post_process(self, doc):
+        pattern = '|'.join([
+            '== References ==',
+            '== Further reading ==',
+            '== External links',
+            '== See also ==',
+            '== Sources ==',
+            '== Notes ==',
+            '== Further references ==',
+            '== Footnotes ==',
+            '=== Notes ===',
+            '=== Sources ===',
+            '=== Citations ===',
+        ])
+        p = re.compile(pattern)
+        indices = [m.start() for m in p.finditer(doc)]
+        min_idx = min(*indices, len(doc))
+        return doc[:min_idx]
+
+class QueryProcessor:
+    
+    def __init__(self, nlp, keep=None):
+        self.nlp = nlp
+        self.keep = keep or {'PROPN', 'NUM', 'VERB', 'NOUN', 'ADJ'}
+
+    def generate_query(self, text):
+        doc = self.nlp(text)
+        query = ' '.join(token.text for token in doc if token.pos_ in self.keep)
+        return query
+
+class PassageRetrieval:
+    
+    def __init__(self, nlp):
+        self.tokenize = lambda text: [token.lemma_ for token in nlp(text)]
+        self.bm25 = None
+        self.passages = None
+
+    def preprocess(self, doc):
+        passages = [p for p in doc.split('\n') if p and not p.startswith('=')]
+        return passages
+
+    def fit(self, docs):
+        passages = list(itertools.chain(*map(self.preprocess, docs)))
+        corpus = [self.tokenize(p) for p in passages]
+        self.bm25 = BM25(corpus)
+        self.passages = passages
+
+    def most_similar(self, question, topn=10):
+        tokens = self.tokenize(question)
+        scores = self.bm25.get_scores(tokens)
+        pairs = [(s, i) for i, s in enumerate(scores)]
+        pairs.sort(reverse=True)
+        passages = [self.passages[i] for _, i in pairs[:topn]]
+        return passages
+
+
+class AnswerExtractor:
+
+    def __init__(self, tokenizer, model):
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        model = AutoModelForQuestionAnswering.from_pretrained(model)
+        self.nlp = QuestionAnsweringPipeline(model=model, tokenizer=tokenizer)
+
+    def extract(self, question, passages):
+        answers = []
+        for passage in passages:
+            try:
+                answer = self.nlp(question=question, context=passage)
+                answer['text'] = passage
+                answers.append(answer)
+            except KeyError:
+                pass
+        answers.sort(key=operator.itemgetter('score'), reverse=True)
+        return answers
+
 def answerQuestionMachine(question):
     context = """
     Machine learning (ML) is the study of computer algorithms that improve automatically through experience. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to do so. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision, where it is difficult or unfeasible to develop conventional algorithms to perform the needed tasks.
--- index.py
+++ index.py
@@ -1,5 +1,25 @@
 from flask import Flask, render_template, request, url_for, redirect
-from QA import answerQuestionMachine
+from QA import answerQuestionMachine, DocumentRetrieval, QueryProcessor, PassageRetrieval, AnswerExtractor
+
+import concurrent.futures
+import itertools
+import operator
+import re
+import os
+import spacy
+
+import requests
+import wikipedia
+from gensim.summarization.bm25 import BM25
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, QuestionAnsweringPipeline
+
+SPACY_MODEL = os.environ.get('SPACY_MODEL', 'en_core_web_sm')
+QA_MODEL = os.environ.get('QA_MODEL', 'distilbert-base-cased-distilled-squad')
+nlp = spacy.load(SPACY_MODEL, disable=['ner', 'parser', 'textcat'])
+query_processor = QueryProcessor(nlp)
+document_retriever = DocumentRetrieval()
+passage_retriever = PassageRetrieval(nlp)
+answer_extractor = AnswerExtractor(QA_MODEL, QA_MODEL)
 
 app=Flask("app")
 
@@ -7,8 +27,15 @@ app=Flask("app")
 def main():
     if request.method=="POST":
         question=request.form["question"]
-        answer, context=answerQuestionMachine(question)
-        return render_template('home.html', ans=answer, ques=question, context=context)
+        """
+        answer, context=answerQuestionMachine(question)"""
+        query = query_processor.generate_query(question)
+        docs = document_retriever.search(query)
+        passage_retriever.fit(docs)
+        passages = passage_retriever.most_similar(question)
+        answers = answer_extractor.extract(question, passages)
+
+        return render_template('home.html', ans=answers[0], ques=question)
     else:
         return render_template('home.html', ans = "", context = "")
 
